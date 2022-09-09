b3822d28a94dda0e6528da46a8d5d7c5148a558b
Patrick Deisinger <patrick.deis@gmail.com>
2022-09-09 12:02:06 +0200

    Extract error and logger

---
+++ errs/errors.go
@@ -0,0 +1,33 @@
+package errs
+
+import "net/http"
+
+type AppError struct {
+	Code    int    `json:",omitempty"`
+	Message string `json:"message"`
+}
+
+func (ae AppError) AsMessage() *AppError {
+	return &AppError{Message: ae.Message}
+}
+
+func NewNotFoundError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusNotFound,
+		Message: message,
+	}
+}
+
+func NewInternalServerError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusInternalServerError,
+		Message: message,
+	}
+}
+
+func NewValidationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnprocessableEntity,
+		Message: message,
+	}
+}
